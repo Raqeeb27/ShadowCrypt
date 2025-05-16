4cfd13bcd2e6273a67a1125a925aad2b9f772698
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2025-05-16 18:32:52 +0530

    feat(rightclick): Add ShadowCrypt icon to the rightclick menu options

--- Set-RightClick.bat
+++ Set-RightClick.bat
@@ -4,6 +4,7 @@ setlocal
 SET shadowcrypt_path=%~dp0dist\ShadowCrypt.exe
 SET hiding_path=%~dp0dist\hiding.exe
 SET recovery_path=%~dp0dist\recovery.exe
+SET shadowcrypt_icon_path=%~dp0icon\ShadowCrypt.ico
 SET sendto_path=%APPDATA%\Microsoft\Windows\SendTo
 SET vbs_file=%TEMP%\create_shortcuts.vbs
 SET hide_shortcut_name=Hide Selected Files.lnk
@@ -15,19 +16,24 @@ echo.
 
 reg add "HKEY_CLASSES_ROOT\*\shell\Hide File" /t REG_SZ /v "" /d "Hide File (Create lnk File)" /f
 reg add "HKEY_CLASSES_ROOT\*\shell\Hide File" /v "MultiSelectModel" /t REG_SZ /d "Single" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\Hide File" /t REG_SZ /v "Icon" /d "%shadowcrypt_icon_path%" /f
 reg add "HKEY_CLASSES_ROOT\*\shell\Hide File\command" /t REG_SZ /v "" /d "\"%shadowcrypt_path%\" hide --files \"%%1\"" /f
 
 reg add "HKEY_CLASSES_ROOT\Lnkfile\shell\Recover File" /t REG_SZ /v "" /d "Recover File (Extract File)" /f
 reg add "HKEY_CLASSES_ROOT\Lnkfile\shell\Recover File" /v "MultiSelectModel" /t REG_SZ /d "Single" /f
+reg add "HKEY_CLASSES_ROOT\Lnkfile\shell\Recover File" /t REG_SZ /v "Icon" /d "%shadowcrypt_icon_path%" /f
 reg add "HKEY_CLASSES_ROOT\Lnkfile\shell\Recover File\command" /t REG_SZ /v "" /d "\"%shadowcrypt_path%\" recover --link_files \"%%1\"" /f
 
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover All" /t REG_SZ /v "" /d "Recover All (Extract All Files)" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover All" /t REG_SZ /v "Icon" /d "%shadowcrypt_icon_path%" /f
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover All\command" /t REG_SZ /v "" /d "\"%shadowcrypt_path%\" recover --all" /f
 
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover Files in This Folder" /t REG_SZ /v "" /d "Recover Files in This Folder" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover Files in This Folder" /t REG_SZ /v "Icon" /d "%shadowcrypt_icon_path%" /f
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover Files in This Folder\command" /t REG_SZ /v "" /d "\"%shadowcrypt_path%\" recover --dir \"%%V\"" /f
 
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover Files Recursively" /t REG_SZ /v "" /d "Recover Files Recursively (Include Subfolders)" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover Files Recursively" /t REG_SZ /v "Icon" /d "%shadowcrypt_icon_path%" /f
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Recover Files Recursively\command" /t REG_SZ /v "" /d "\"%shadowcrypt_path%\" recover --recursive --dir \"%%V\"" /f
 
 
