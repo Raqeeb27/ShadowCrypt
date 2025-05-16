e97f2764bfd6c0c0aca04a25f047be742a02f26e
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2025-05-16 16:10:01 +0530

    feat(install): Improve user experience by adding database reinitialization confirmation

--- install.ps1
+++ install.ps1
@@ -7,15 +7,28 @@ echo "`n---------------------------------`n"
 
 
 echo "[*] Initializing databases.`n"
-$output = uv run init_db.py | ForEach-Object { Write-Host $_; $_ }
-if ($output) {
-    if ($output[-1] -eq "[-] PASSWORD ERROR" -or $output[-1] -eq "[-] Keyboard Interrupt" -or $output[-1] -eq "[-] PATH ERROR") {
-        Write-Host "[!] Installation Failed!!!"
-        exit 1
-    }
+if ((Test-Path "db\enc_mapping.dll") -and (Test-Path "db\app_path.dll")) {
+    $confirmation = Read-Host "Do you want to reinitialize the database?`nWarning: This will overwrite the existing databases.`nType 'yes' to continue "
+} else {
+    $confirmation = "yes"
+}
+
+if ($confirmation -ne "yes" -and $confirmation -ne "y") {
+    Write-Host "`n[!] Aborting database initialization."
 }
 else {
-    Write-Host "`n[+] Databases initialization successfull."
+    Write-Host
+    $output = uv run init_db.py | ForEach-Object { Write-Host $_; $_ }
+    if ($output) {
+        if ($output[-1] -eq "[-] PASSWORD ERROR" -or $output[-1] -eq "[-] Keyboard Interrupt" -or $output[-1] -eq "[-] PATH ERROR") {
+            Write-Host "[!] Installation Failed!!!"
+            Read-Host "`nPress any key to exit"
+            exit 1
+        }
+    }
+    else {
+        Write-Host "`n[+] Databases initialization successfull."
+    }
 }
 
 echo "`n---------------------------------`n"
@@ -27,10 +40,11 @@ if (Test-Path "dist") {
     Remove-Item -Recurse -Force dist
 }
 
-uv run pyinstaller -F ShadowCrypt.py --uac-admin --manifest admin.manifest
-uv run pyinstaller -F hiding.py
-uv run pyinstaller -F recovery.py
-uv run pyinstaller -F linker.py
+uv run pyinstaller -F ShadowCrypt.py --uac-admin --manifest admin.manifest --icon=icon\ShadowCrypt.ico
+uv run pyinstaller -F hiding.py --icon=icon\ShadowCrypt.ico
+uv run pyinstaller -F recovery.py --icon=icon\ShadowCrypt.ico
+uv run pyinstaller -F linker.py --icon=icon\ShadowCrypt.ico
+uv run pyinstaller -F init_db.py --icon=icon\ShadowCrypt.ico --distpath=.
 echo "`n---------------------------------`n"
 
 
